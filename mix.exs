6611c10af0eb9666a8c529c22756d3a0d5aa4718
Alexander Afanasyev <alexander.afanasyev@aviasales.ru>
2023-03-06 23:08:16 +0800

    Introduce Mix project configuration

---
+++ mix.exs
@@ -0,0 +1,45 @@
+defmodule WithTimeout.MixProject do
+  use Mix.Project
+
+  @name "WithTimeout"
+  @version "0.1.0"
+  @description "Both total and time limited evaluation of expressions"
+  @repo_url "https://github.com/iamafanasyev/with_timeout"
+
+  def project do
+    [
+      app: :with_timeout,
+      version: @version,
+      elixir: "~> 1.2",
+      deps: deps(),
+      # Hex
+      description: @description,
+      package: package(),
+      # ExDoc
+      name: @name,
+      source_url: @repo_url,
+      docs: docs()
+    ]
+  end
+
+  defp deps() do
+    [
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: @name,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      maintainers: ["Aleksandr Afanasev"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
+    ]
+  end
+end
